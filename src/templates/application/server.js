ab21c4d51659a4841208f286ee3510347d3584d6
Anders Evenrud <andersevenrud@gmail.com>
2019-01-01 21:20:35 +0100

    Updated application scaffolding template

--- src/templates/application/server.js
+++ src/templates/application/server.js
@@ -20,4 +20,9 @@ module.exports = (core, proc) => ({
 
   // When server goes down
   destroy: () => {},
+
+  // When using an internally bound websocket, messages comes here
+  onmessage: (ws, respond, args) => {
+    respond('Pong');
+  }
 });
