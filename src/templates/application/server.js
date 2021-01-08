f1f14ce2ecd3ce9cbd507fa213c5ca0240dbd0b7
Anders Evenrud <andersevenrud@gmail.com>
2021-01-08 01:50:53 +0100

    Updated application server example

--- src/templates/application/server.js
+++ src/templates/application/server.js
@@ -3,7 +3,7 @@ module.exports = (core, proc) => {
 
   return {
     // When server initializes
-    init: async () => {
+    async init() {
       // HTTP Route example (see index.js)
       routeAuthenticated('POST', proc.resource('/test'), (req, res) => {
         res.json({hello: 'World'});
@@ -17,13 +17,15 @@ module.exports = (core, proc) => {
     },
 
     // When server starts
-    start: () => {},
+    async start() {
+    },
 
     // When server goes down
-    destroy: () => {},
+    destroy() {
+    },
 
     // When using an internally bound websocket, messages comes here
-    onmessage: (ws, respond, args) => {
+    onmessage(ws, respond, args) {
       respond('Pong');
     }
   };
